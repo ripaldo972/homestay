c1ded1748c1e81a231a15f94c27dcd5e52fa3001
ripaldo972 <williamsteven12122002@gmail.com>
2024-07-12 07:20:47 +0700

    Setting API

--- lib/pages/api_provider.dart
+++ lib/pages/api_provider.dart
@@ -1,5 +1,22 @@
+import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+
+class Product {
+  final String name;
+  final String address;
+  final double price;
+
+  Product({required this.name, required this.address, required this.price});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      name: json['name'],
+      address: json['address'],
+      price: json['price'],
+    );
+  }
+}
 
 class ApiService {
   static const String baseUrl =
@@ -10,7 +27,13 @@ class ApiService {
     return await http.get(url);
   }
 
-  // Metode lainnya untuk post, update, delete sesuai kebutuhan
+  Future<Product> fetchProductDetail(String id) async {
+    final response =
+        await getData('products/$id'); // Ganti dengan endpoint Anda
+    if (response.statusCode == 200) {
+      return Product.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load product');
+    }
+  }
 }
-
-final apiServiceProvider = Provider((ref) => ApiService());
